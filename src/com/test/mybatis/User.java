3b528e0f5d09068dfd042469314a289aed549327
zwh <zwh@192.168.86.1>
2019-04-22 16:59:04 +0800

    @@@

--- src/com/test/mybatis/User.java
+++ src/com/test/mybatis/User.java
@@ -87,6 +87,6 @@ public class User implements Serializable {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", username=" + username + ", sex=" + sex + ", birthday=" + birthday + ", address="
-				+ address + ",custom="+custom.getCustomName()+"]";
+				+ address + "]";
 	}
 }
